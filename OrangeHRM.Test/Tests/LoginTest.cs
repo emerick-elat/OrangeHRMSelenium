46a8d1b2465ec6476a230d67a28430b58a885250
Aymerick Fikeuelat <Aymerick.Fikeuelat@ventionteams.com>
2024-10-23 18:55:31 +0300

    ValidateSuccesfullLogin

--- OrangeHRM.Test/Tests/LoginTest.cs
+++ OrangeHRM.Test/Tests/LoginTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OrangeHRM.Test.PageObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,43 @@ using System.Threading.Tasks;
 
 namespace OrangeHRM.Test.Tests
 {
+    [TestFixture]
     internal class LoginTest
     {
+        private LoginPage _loginPage;
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = BrowserFactory.CreateBrowser(BrowserType.Chrome);
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl("https://opensource-demo.orangehrmlive.com/");
+            _loginPage = new LoginPage(_driver);
+            Thread.Sleep(1000);
+        }
+
+        [Test]
+        public void ValidateSuccesfullLogin()
+        {
+            Assert.That(_driver.Title, Is.EqualTo("OrangeHRM"));
+            Assert.IsTrue(_loginPage.LoginPageDoOpen());
+
+            _loginPage.SetUsername(LoginData.Username);
+            Assert.That(_loginPage.UsernameField.GetAttribute("value"), Is.EqualTo(LoginData.Username));
+
+            _loginPage.SetPassword(LoginData.Password);
+            Assert.That(_loginPage.PasswordField.GetAttribute("value"), Is.EqualTo(LoginData.Password));
+
+            _loginPage.LoginButtonClicked();
+
+            Assert.IsTrue( _loginPage.IsLoggedIn());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Close();
+        }
     }
 }
